19560ba62a46d747a9859fa5f3663e7a933e48d6
Sergi Coma Corcuera <scomacorcuera@iesesteveterradas.cat>
2017-12-04 20:19:21 +0100

    Email

--- paginas/enviar.php
+++ paginas/enviar.php
@@ -1,14 +1,18 @@
 <?php
-function enviarInvitacion($correo,$idUsuario,$idConsulta){
-	$qstr= "INSERT INTO `Invitacion`(`ID_Usuario`, `ID_Consulta`)  VALUES ('$idUsuario','$idConsulta')";
-	$query=$con->prepare($qstr);
-	$query->execute();
-	$e= $query->errorInfo();
-	if ($e[0]!='00000') {
-		die("Error accedint a dades: " . $e[2]);
-	}
-	$mensaje = "Estimado/a usuario nos gustaria invitarte a nuestra pagina para que puedas votar a una serie de cuestiones.";
-	$mensaje = wordwrap($mensaje, 70, "\r\n");
-	mail($correo, 'Invitacion Project Vota', $mensaje);
+session_start()
+$userid = $_SESSION['idUsuario'];
+$correo = $_POST['correo'];
+$idConsulta = $_POST['idConsulta'];
+
+$qstr= "INSERT INTO `Invitacion`(`ID_Usuario`, `ID_Consulta`)  VALUES ('$idUsuario','$idConsulta')";
+$query=$con->prepare($qstr);
+$query->execute();
+$e= $query->errorInfo();
+if ($e[0]!='00000') {
+	die("Error accedint a dades: " . $e[2]);
+}
+$mensaje = "Estimado/a usuario nos gustaria invitarte a nuestra pagina para que puedas votar a una serie de cuestiones.";
+$mensaje = wordwrap($mensaje, 70, "\r\n");
+mail($correo, 'Invitacion Project Vota', $mensaje);
 }
 ?>
